de4991a076f5ae21cac3a8bba71c0332dc26380a
fajarfadillahh <fajaragustian979@gmail.com>
2021-05-06 21:19:27 +0700

    add gsap animation

--- src/js/script.js
+++ src/js/script.js
@@ -35,3 +35,49 @@ function move(e) {
     layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
   });
 }
+
+// GSAP ANIMATION
+// === Header Section ===
+gsap.from(".nav__logo, .nav__toggle", {
+  opacity: 0,
+  duration: 1,
+  delay: 0.4,
+  y: 10,
+});
+
+gsap.from(".nav__item", {
+  opacity: 0,
+  duration: 1,
+  delay: 0.8,
+  y: 30,
+  stagger: 0.2,
+});
+
+// === Home Section ===
+gsap.from(".home__title", {
+  opacity: 0,
+  duration: 1.4,
+  delay: 1.2,
+  y: 30,
+});
+
+gsap.from(".home__text", {
+  opacity: 0,
+  duration: 1,
+  delay: 1.6,
+  y: 30,
+});
+
+gsap.from(".home__button", {
+  opacity: 0,
+  duration: 1,
+  delay: 2,
+  y: 30,
+});
+
+gsap.from(".home__img", {
+  opacity: 0,
+  duration: 1,
+  delay: 1.5,
+  y: 30,
+});
